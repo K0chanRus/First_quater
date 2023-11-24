c88c1eba1bc24df3b21f413fa51cad545bf63675
K0chanRus <144388877+K0chanRus@users.noreply.github.com>
2023-11-24 09:13:02 +0300

    end programm

--- Program.cs
+++ Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] CreatFirstArray(int size)
+{
+    string[] arr = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = Convert.ToString(Console.ReadLine());
+    }
+    return arr;
+}
+
+string[] SortingThreeDigitNumbers(string[] firstArray, int size)
+{
+    string[] firstSortingArray = new string[size];
+    int longArr = 0;
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+        if (firstArray[i].Length <= 3)
+        {
+            firstSortingArray[longArr] = firstArray[i];
+            longArr++;
+        }   
+    }
+    string[] sortingArray = new string[longArr];
+    for (int j = 0; j < sortingArray.Length; j++)
+    {
+        sortingArray[j] = firstSortingArray[j];
+    }
+    return sortingArray;
+}
+
+// Написать удаление последних  не использованных строк!!!
+
+void PrintArray(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"'{arr[i]}', ");
+        else Console.Write($"'{arr[i]}'");
+    }
+    Console.Write("]");
+}
+
+Console.WriteLine("Сколько строк будет в массиве?");
+int row = Convert.ToInt32(Console.ReadLine());
+string [] firstArr = CreatFirstArray(row);
+PrintArray(firstArr);
+Console.Write(" => ");
+string [] lastArr = SortingThreeDigitNumbers(firstArr, row);
+PrintArray(lastArr);
